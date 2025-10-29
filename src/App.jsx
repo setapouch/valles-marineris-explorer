caf7232c82956bb8084fba6d3592656b53bf544c
setapouch <radimjelinek@gmail.com>
2025-10-29 22:12:09 +0100

    feat: add MarsInfo component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MarsInfo from './MarsInfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(100)
 
   return (
     <>
@@ -16,7 +17,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Valles marineris exporer </h1>
+      <MarsInfo />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/MarsInfo.jsx
@@ -0,0 +1,10 @@
+function MarsInfo() {
+  return (
+    <div>
+      <h2>Valles Marineris</h2>
+      <p>Největší kaňonový systém ve sluneční soustavě, nacházející se na Marsu. Je hluboký až 10 km.</p>
+    </div>
+  );
+}
+
+export default MarsInfo;
