13961566c625342306e7fc4bc5b812efe3f8308f
setapouch <radimjelinek@gmail.com>
2025-11-02 16:04:45 +0100

    Install Tailwind (failed)

--- src/MarsInfo.jsx
+++ src/MarsInfo.jsx
@@ -1,8 +1,8 @@
-function MarsInfo({title = "Neznámá oblast", description = "Popis není k dispozici." , ...props}) {
+function MarsInfo({title = "Neznámá oblast", description = "Popis není k dispozici."}) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
+    <div className="bg-gray-800 text-white p-6 rounded-lg shadow-lg mb-4 hover:bg-gray-700 transition-colors">
+      <h2 className="text-2xl font-bold mb-2 text-orange-400">{title}</h2>
+      <p className="text-gray-300">{description}</p>
     </div>
   )
 }
